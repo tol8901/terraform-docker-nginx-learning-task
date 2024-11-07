6262b46c6f638fbfa64f33770638f3e97c36b345
Anatolii_Kravchenko <anatolii.kravchenko@zaelab.com>
2024-11-07 12:44:03 +0100

    [006] Removed aws_eip

--- vpc.tf
+++ vpc.tf
@@ -34,16 +34,11 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
 
   tags = {
     Name = "main-nat"
   }
-}
-
-resource "aws_eip" "nat" {
-  associate_with_private_ip = true
 }
 
 resource "aws_route_table" "public" {
