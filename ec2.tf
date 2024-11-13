3656055388075c79bf907129c84ff9cd1b46c3e0
Anatolii_Kravchenko <anatolii.kravchenko@zaelab.com>
2024-11-13 18:37:06 +0100

    [037] Uncommented the EC2 resource definition

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
-# resource "aws_instance" "example" {
-#   ami           = "ami-006dcf34c09e50022" // Amazon Linux 2 AMI
-#   instance_type = "t2.micro"
-#   vpc_security_group_ids = [
-#     aws_security_group.example.id,
-#   ]
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               yum install -y docker
-#               systemctl enable docker
-#               systemctl start docker
-#               sudo chown $USER /var/run/docker.sock
-#               docker image pull justwatchcomearring331/nginx-14:1.0
-#               docker run -d -p 8081:80 -e EXTERNAL_PORT=8081 justwatchcomearring331/nginx-14:1.0
-#               EOF
-# }
+resource "aws_instance" "example" {
+  ami           = "ami-006dcf34c09e50022" // Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    aws_security_group.example.id,
+  ]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y docker
+              systemctl enable docker
+              systemctl start docker
+              sudo chown $USER /var/run/docker.sock
+              docker image pull justwatchcomearring331/nginx-14:1.0
+              docker run -d -p 8081:80 -e EXTERNAL_PORT=8081 justwatchcomearring331/nginx-14:1.0
+              EOF
+}
