53ff6dcff382d2f1779bc5d4589052b3ac59a436
Anatolii_Kravchenko <anatolii.kravchenko@zaelab.com>
2024-11-13 13:04:39 +0100

    [031] The DynamoDB is changed

--- DynamoDB.tf
+++ DynamoDB.tf
@@ -1,10 +1,16 @@
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
+  billing_mode = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key = "LockID"
+  
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  tags = {
+    Name = "terraform-up-and-running-locks"
   }
 }
